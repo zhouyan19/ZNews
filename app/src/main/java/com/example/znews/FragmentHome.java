6794b33fcaaacf7965d86da116a776625189e011
周 <zhouyan19@mails.tsinghua.edu.cn>
2021-08-10 09:33:05 +0800

    signed apk

--- app/src/main/java/com/example/znews/FragmentHome.java
+++ app/src/main/java/com/example/znews/FragmentHome.java
@@ -7,23 +7,29 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.target.Target;
+import com.scwang.smart.refresh.footer.ClassicsFooter;
+import com.scwang.smart.refresh.header.ClassicsHeader;
+import com.scwang.smart.refresh.layout.api.RefreshLayout;
+import com.scwang.smart.refresh.layout.listener.OnRefreshListener;
 
+import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -32,55 +38,36 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class FragmentHome extends Fragment {
-    private int news_cnt = 0;
     protected ArrayList<News> all_news;
 
-    class MyAdapter extends BaseAdapter {
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
+        @NonNull
+        @NotNull
         @Override
-        public int getCount() {
-            return all_news.size();
+        public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_item, parent, false);
+            return new ViewHolder(v);
         }
 
+        @SuppressLint("UseCompatLoadingForDrawables")
         @Override
-        public Object getItem(int position) {
-            return all_news.get(position);
-        }
+        public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+            holder.news_item.setOnClickListener(v -> showDetail(all_news.get(position)));
 
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @SuppressLint({"InflateParams", "UseCompatLoadingForDrawables"})
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view;
-
-            if (convertView == null) {
-                // sx: 这里通过 parent.getContext() 得到父对象的上下文，以填充 food_item。
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_item, null);
-            } else {
-                view = convertView;
-            }
-
-            LinearLayout news_item = view.findViewById(R.id.news_item);
-            news_item.setOnClickListener(v -> showDetail(all_news.get(position)));
-
-            TextView news_title = view.findViewById(R.id.news_title);
             String title = (String) all_news.get(position).getTitle();
             if (title != null) {
                 if (title.length() > 20) title = title.substring(0, 20) + "...";
             } else {
                 title = "";
             }
-            news_title.setText(title);
+            holder.news_title.setText(title);
 
-            ImageView news_pic = view.findViewById(R.id.news_pic);
             String picUrl = all_news.get(position).getPicUrl();
-            Glide.with(view)
+            Glide.with(holder.itemView)
                     .load(picUrl)
                     .centerCrop()
                     .dontTransform()
@@ -88,22 +75,39 @@ public class FragmentHome extends Fragment {
                     .format(DecodeFormat.PREFER_ARGB_8888)
                     .diskCacheStrategy(DiskCacheStrategy.ALL)
                     .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
-                    .into(news_pic);
+                    .into(holder.news_pic);
 
-            ImageButton favor_button = view.findViewById(R.id.favor_button);
-            favor_button.setOnClickListener(v -> {
+            holder.favor_button.setOnClickListener(v -> {
                 boolean favor = all_news.get(position).getFav();
                 if (favor) {
-                    favor_button.setImageDrawable(getResources().getDrawable(R.drawable.fav_grey_foreground));
+                    holder.favor_button.setImageDrawable(getResources().getDrawable(R.drawable.fav_grey_foreground));
                     all_news.get(position).setFav(false);
                 }
                 else {
-                    favor_button.setImageDrawable(getResources().getDrawable(R.drawable.fav_foreground));
+                    holder.favor_button.setImageDrawable(getResources().getDrawable(R.drawable.fav_foreground));
                     all_news.get(position).setFav(true);
                 }
             });
+        }
 
-            return view;
+        @Override
+        public int getItemCount() {
+            return all_news.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            LinearLayout news_item;
+            TextView news_title;
+            ImageView news_pic;
+            ImageButton favor_button;
+
+            public ViewHolder(@NonNull @NotNull View itemView) {
+                super(itemView);
+                news_item = itemView.findViewById(R.id.news_item);
+                news_title = itemView.findViewById(R.id.news_title);
+                news_pic = itemView.findViewById(R.id.news_pic);
+                favor_button = itemView.findViewById(R.id.favor_button);
+            }
         }
     }
 
@@ -122,37 +126,39 @@ public class FragmentHome extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        SwipeRefreshLayout srl = view.findViewById(R.id.swipe_refresh);
-        srl.setColorSchemeResources(R.color.royal_blue);
-        srl.setOnRefreshListener(() -> {
+        RefreshLayout srl = view.findViewById(R.id.smart_refresh);
+        srl.setRefreshHeader(new ClassicsHeader(requireContext()));
+        srl.setRefreshFooter(new ClassicsFooter(requireContext()));
+        srl.setOnRefreshListener(refreshLayout -> {
             try {
-                news_cnt = 0;
                 initData();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            srl.setRefreshing(false);
+            refreshLayout.finishRefresh(3000);
+        });
+        srl.setOnLoadMoreListener(refreshLayout -> {
+            try {
+                Log.e("LoadMoreListener", "entering moreData");
+                moreData(all_news.size());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            refreshLayout.finishLoadMore(3000);
         });
 
-        srl.setRefreshing(true);
         try {
-            news_cnt = 0;
             initData();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        srl.setRefreshing(false);
 
-        ListView lv_list = view.findViewById(R.id.lv_list);
-        lv_list.setAdapter(new MyAdapter());
-        Log.e("MainTread", "Adapter set");
+        RecyclerView rv_list = view.findViewById(R.id.rv_list);
+        rv_list.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
+        rv_list.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv_list.setAdapter(new MyAdapter());
 
         super.onViewCreated(view, savedInstanceState);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
     }
 
     public void initData () throws InterruptedException {
@@ -181,9 +187,11 @@ public class FragmentHome extends Fragment {
     }
 
     private void getAllNews (int cnt) throws IOException {
+        cnt += 10;
         String head = "https://olympics.com/tokyo-2020/zh/library/editorial/country/all/sport/all/order/desc/skip/";
         String tail = "/limit/10/morenews-grid";
         String root = head + cnt + tail;
+        Log.e("SubThread/getNews", root);
         Log.e("SubThread/getNews", "Trying to get the html...");
         Document document = Jsoup.parse(new URL(root).openStream(), "UTF-8", root);
         Log.e("SubThread/getNews", "Document!");
@@ -196,17 +204,19 @@ public class FragmentHome extends Fragment {
                 String href = a.attr("href");
                 if (href.equals("")) continue;
                 news.setUrl(href);
+                Log.e("Cards_Item/url", href);
                 Element article = a.children().select("article").first();
                 Element img = article.children().select("figure").first().children().select("picture").first().children().select("img").first();
                 String picUrl = img.attr("data-src");
                 if (picUrl.equals("")) continue;
                 news.setPicUrl(picUrl);
+                Log.e("Cards_Item/picUrl", picUrl);
                 Element h3 = article.children().select("div").first().children().select("header").first().children().select("div").first().children().select("h3").first();
                 String title = h3.attr("title");
                 if (title.equals("")) continue;
                 news.setTitle(title);
+                Log.e("Cards_Item/title", title);
                 all_news.add(news);
-                news_cnt++;
             } catch (Exception e) {
                 e.printStackTrace();
             }
